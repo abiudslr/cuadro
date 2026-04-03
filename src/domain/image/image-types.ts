3af646d71de127b4e5375af9d063d54a41621407
Abiud Salazar <ab-iud@hotmail.com>
2026-04-03 10:22:35 -0600

    se borra todo

--- src/domain/image/image-types.ts
+++
@@ -1,8 +0,0 @@
-export type ImageAsset = {
-    id: string;
-    file: File;
-    name: string;
-    width: number;
-    height: number;
-    objectUrl: string;
-};
--- src/lib/ids.ts
+++
@@ -1,3 +0,0 @@
-export function createId(prefix = "id"): string {
-    return `${prefix}-${crypto.randomUUID()}`;
-}
